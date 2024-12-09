e22abb40e48980b1a5e26b6739375e8b8a5d24ba
Lavina-23 <lavinakohai3227@gmail.com>
2024-12-09 09:47:04 +0700

    fix: empty mapres condition

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -5,17 +5,22 @@ class HomeController extends Controller
   public function index()
   {
     $data['leaderboard'] = $this->model('Mapres')->getTopThreeMapres();
-    $order = [
-      $data['leaderboard'][1],
-      $data['leaderboard'][0],
-      $data['leaderboard'][2],
-    ];
-
-    $data['leaderboard'] = $order;
 
     $this->view("layout/header");
     $this->view("layout/navbar");
-    $this->view("home/index", $data);
+
+    if (!empty($data['leaderboard'])) {
+      $order = [
+        $data['leaderboard'][1],
+        $data['leaderboard'][0],
+        $data['leaderboard'][2],
+      ];
+
+      $data['leaderboard'] = $order;
+      $this->view("home/index", $data);
+    } else {
+      $this->view("home/index");
+    }
     $this->view("layout/footer");
   }
 
